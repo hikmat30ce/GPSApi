686c68d0dc56bcdeab0d6d3bf98aab908b633d91
Hikmat Ullah <hikmat30ce@yahoo.com>
2018-05-29 21:02:05 +0500

    initial commit

---
+++ src/main/java/com/mycompany/gpsapi/GreetingController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gpsapi;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author HUK91
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public void greeting(@RequestBody String inputdata) {
+        System.out.println(inputdata);
+    }
+}
